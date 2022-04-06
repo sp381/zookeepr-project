481bc4b8708020cc84fb88022e4c480468e289bf
sp381 <sarah.powell381@gmail.com>
2022-04-06 14:53:07 -0700

    added if/else

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
   }
-  
+
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
@@ -47,10 +47,14 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-  app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-      res.json(result);
-  });
+ app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 app.listen(PORT, () => {
     console.log('API server now on port ${PORT}!');
